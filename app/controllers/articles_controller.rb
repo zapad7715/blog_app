b3efd3856c4dd9f4620dfa0f2db1b6cf6e12107d
zapad7715 <zapad7715@ya.ru>
2016-01-22 00:37:26 +0300

    Add view 'new' for Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
   def index
   end
+
+  def new
+    @article = Article.new
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160121205958) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "articles", ["title"], name: "index_articles_on_title"
 
 end

125891f6582c5247aab988adc9ebd472caeeea5a
zapad7715 <zapad7715@ya.ru>
2016-02-01 14:01:34 +0300

    Link articles to users

--- spec/features/editing_article_spec.rb
+++ spec/features/editing_article_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.feature "Editing an Article" do
   
   before do
-    @article = Article.create(title: 'First article', body: 'Body of first article')
+    john = User.create(email: 'john@example.com', password: 'password')
+    login_as(john)
+    @article = Article.create(title: 'First article', body: 'Body of first article', user: john)
   end
 
   scenario "A user updates an article" do
